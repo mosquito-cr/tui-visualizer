f6d7b3e5d9bae9921c9ae082cce29c89c0d92fac
rob <robacarp@users.noreply.github.com>
2022-07-15 16:50:49 -0600

    having some fun with colorizing the runner status

--- src/interface/inspector_addons/runner.cr
+++ src/interface/inspector_addons/runner.cr
@@ -3,10 +3,26 @@ module Mosquito::Inspector
     def last_active : String
       if timestamp = last_heartbeat
         seconds = (Time.utc - timestamp).total_seconds.to_i
-        "seen #{seconds}s ago"
+
+        if seconds < 21
+          colorize_by_last_heartbeat seconds, "online"
+        else
+          colorize_by_last_heartbeat seconds, "seen #{seconds}s ago"
+        end
+
       else
-        "heartbeat expired"
+        colorize_by_last_heartbeat 301, "expired"
       end
+    end
+
+    def colorize_by_last_heartbeat(seconds : Int32, word : String) : String
+      if seconds < 30
+        word.colorize(:green)
+      elsif seconds < 200
+        word.colorize(:yellow)
+      else
+        word.colorize(:red)
+      end.to_s
     end
 
     def status : String
